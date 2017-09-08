0a01603ffda796a94d7e154128975a51a05170eb
ZetDude <31309189+ZetDude@users.noreply.github.com>
2017-09-08 22:35:05 +0300

    re-phrased k!about

--- commands/about.py
+++ commands/about.py
@@ -12,9 +12,9 @@ handle2 = loader2.load_module('basic')
 def run(message, prefix, alias):
     nPrefix = handle.prefix
     rpgPrefix = handle2.rpgPrefix
-    aboutText = """```
+    aboutText = """
 Hi! I am KalevBot, a bot designed specifically for this server!
-I was designed by ZetDude, and I consist of 100% spaghetti.
+I was designed by ZetDude, with a lot of help from xithiox, and I consist of 100% spaghetti.
 I am here to help with the relay managment and some other minor things.
 I also have a little text RPG-battle-explore-dungeon-rogue-like thing, whatever that is.
 To learn about that, use <{0}about> instead.
@@ -22,7 +22,10 @@ But what are my command, you might wonder?
 Just type <{1}help> to see!
 
 I am made in python using the discord.py API wrapper.
-```""".format(nPrefix, rpgPrefix)
+You can help develop the bot at:
+https://github.com/ZetDude/KALEVBOT/
+Thanks for all the support!
+""".format(nPrefix, rpgPrefix)
     return "m", [message.channel, aboutText]
 
 def help_use():
