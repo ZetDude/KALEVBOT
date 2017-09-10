717eec4ca595c5c24bd7cea4bef85487ab3cb564
ZetDude <31309189+ZetDude@users.noreply.github.com>
2017-09-10 14:45:35 +0300

    updated _rename_me_to_obot.py_

---
+++ _rename_me_to_obot.py_.py
@@ -0,0 +1,10 @@
+#token goes here
+token = ''
+allowedChannel = ['channelID', 'channelID2', 'etc']
+allowedServer = ['serverID', 'etc']
+delMsg = "insert what to say when someone deletes something"
+cooldown = 1 #after x amount of seconds, delete the delete message shown above
+botPrefix = "m!" #what prefix to use for BOT commands
+rpgPrefix = "mr!" #what prefix to use from RPG commands
+name = "We need more bots" #bot name
+game = "aaaaaa" #'playing' text
