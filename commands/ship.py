eb9fd3ae382f695a902a0aef9cbb55877939b138
ZetDude <31309189+ZetDude@users.noreply.github.com>
2017-09-10 09:59:05 +0300

    added automatic shipname generation

--- commands/ship.py
+++ commands/ship.py
@@ -16,7 +16,7 @@ def run(message, prefix, alias):
     seen = set()
     seen_add = seen.add
     ships = [x for x in ships if not (x in seen or seen_add(x))]
-    if len(ships) == 0:
+    if not ships:
         return "m", [message.channel, message.author.mention + ", how does one ship nobody? Mention at least two people in the message"]
     elif len(ships) == 1:
         return "m", [message.channel, message.author.mention + ", they arent that lonely. Mention at least two people in the message"]
@@ -37,6 +37,14 @@ def run(message, prefix, alias):
     lines[shipAdd] = occ
     with open(shipfile, 'wb') as f: 
         pickle.dump(lines, f)
+        
+    if len(ships) == 2:
+        half1 = len(shipsM[0]) // 2
+        half2 = len(shipsM[1]) // 2
+        com1 = shipsM[0][:half1]
+        com2 = shipsM[1][-half2:]
+        final = com1 + com2
+        finalMSG += "\nI shall call it **" + final + "**"
     
     return "m", [message.channel, finalMSG] 
 
