13409903b4f423e96c2c7ea49c4526ca3e45e018
ZetDude <31309189+ZetDude@users.noreply.github.com>
2017-10-01 00:16:16 +0300

    added capability to use emotes from other server via the bot

--- commands/emote.py
+++ commands/emote.py
@@ -1,6 +1,7 @@
 import importlib.machinery
 import os
 import sys
+import discord
 
 sp = os.path.dirname(os.path.realpath(sys.argv[0]))
 
@@ -8,9 +9,14 @@ loader2 = importlib.machinery.SourceFileLoader('maincore', sp + '/maincore.py')
 core = loader2.load_module('maincore')
 
 def run(message, prefix, alias):
-
-    emotes = ''.join([str(x) for x in core.cl.get_all_emojis()])
-    core.send(message.channel, emotes)
+    if message.content.strip() == prefix + alias:
+        emotes = ''.join([str(x) for x in core.cl.get_all_emojis()])
+        core.send(message.channel, emotes)
+    else:
+        cmdlen = len(prefix + alias)
+        opstring = message.content[cmdlen:].strip()
+        pos = discord.utils.get(core.cl.get_all_emojis(), name=opstring)
+        core.send(message.channel, pos)
 
 def help_use():
     return "Display the relay deadine"
