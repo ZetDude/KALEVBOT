b6ffa8ed8bbae88141456cc9a90d0c9ec6114b4f
ZetDude <kalevbot@gmail.com>
2017-12-20 10:54:25 +0000

    tweaked about message

--- commands/about.py
+++ commands/about.py
@@ -14,17 +14,15 @@ def run(message, prefix, aliasName):
     rpgPrefix = rpg.rpgPrefix
     aboutText = """
 Hi! I am KalevBot, a bot designed specifically for this guild!
-I was designed by ZetDude, with a lot of help from xithiox, and I consist of 100% spaghetti.
+I was initially created by ZetDude, and I consist of 100% spaghetti.
 I am here to help with the relay managment and some other minor things.
-I also have a little text RPG-battle-explore-dungeon-rogue-like thing, whatever that is.
-To learn about that, use <{1}about> instead.
 But what are my commands, you might wonder?
 Just type <{0}help> to see!
 
 I am made in python using the discord.py API wrapper.
 You can help develop the bot at:
 <https://github.com/ZetDude/KALEVBOT/>
-Thanks for all the support!
+Thanks to xithiox and pecan for the help they have already provided!
 """.format(nPrefix, rpgPrefix)
     core.send(message.channel, aboutText)
 
