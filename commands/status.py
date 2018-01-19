43190fdff609f06aec7fcbf0765a10e7c497840c
ZetDude <kappathemaster@gmail.com>
2018-01-19 12:58:13 +0000

    Updated bot to work on linux

--- commands/status.py
+++ commands/status.py
@@ -9,7 +9,7 @@ core = loader.load_module('maincore')
 
 def run(message, prefix, aliasName):
     difference = core.get_timer()
-    diskspace = core.get_free_space_mb("C:")
+    diskspace = core.get_free_space_mb("/")
     diskspaceg = diskspace / 1024 / 1024 / 1024
     p_working = "It's working! I have been running for " + str(difference)
     p_space = "\nApproximate disk space left for bot: " + str(diskspaceg) + " GB (" + str(diskspace) + " bytes)"
