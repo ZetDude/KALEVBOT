987d6a591dbfda1c661e8d9fc15bcba2b0103750
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-04-13 13:14:35 +0300

    SeachCog → SearchGog

--- cogs/search.py
+++ cogs/search.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import wikipedia
 
 
-class SeachCog():
+class SearchCog():
     "Includes commands about searching the internet"
     def __init__(self, bot):
         self.bot = bot
