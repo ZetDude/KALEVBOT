67a3b5169cdf78a04263eb5d45ef51d6740e8674
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-04-20 21:00:23 +0300

    continued work on fun cog

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,7 @@
 from random import randint
 
 from discord.ext import commands
+from lib import shipname as improved_shipname
 
 
 class FunCog():
@@ -9,7 +10,8 @@ class FunCog():
         self.bot = bot
         type(self).__name__ = "Fun Commands"
 
-    @commands.command(name='night', aliases=['n', 'goodnight', 'nacht', 'öö', 'ööd', 'oyasumi', 'おやすみ'],
+    @commands.command(name='night', aliases=['n', 'goodnight', 'nacht', 'öö', 'ööd', 'oyasumi',
+    'おやすみ'],
                       help=(r"Wish someone a good night using a super cute kaomoji ^_^"),
                       brief="Wish someone a good night.")
     async def night(self, ctx, *, target_user=None):
@@ -26,8 +28,8 @@ class FunCog():
             combine = selected_kaomoji + " Good night!"
         elif target_user == "-list":
             combine = ""
-                for i in kaomoji:
-                    combine = combine + i + "\n"
+            for i in kaomoji:
+                combine = combine + i + "\n"
         else:
             try:
                 target_user = await commands.MemberConverter(ctx, target_user)
@@ -36,6 +38,18 @@ class FunCog():
                 combine = selected_kaomoji + " Thank you, " + target_user + "!"
 
         ctx.send(combine)
+
+    @commands.command(name='developer', aliases=['dev'],
+                      help="Try it!",
+                      brief="Display the best developer of 2017")
+    async def developer(self, ctx):
+        ctx.send("ZetDude best developer of 2017 and 2018 <:zetdev:357193244679077890>")
+
+    @commands.command(name='shipname', aliases=['name'],
+                      help="Create the shipname of two people.")
+    async def shipname(self, ctx, name1, name2):
+        names_shipname = improved_shipname.shipname(name1, name2)
+        ctx.send("\nI shall call it \"**" + names_shipname + "**\"!")
 
 def setup(bot):
     bot.add_cog(FunCog(bot))
