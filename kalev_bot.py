6f0b33bd475f35f9b23306ad3301aef6edf30f83
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-05-01 18:51:05 +0300

    removed maincore import which doesn't exist anymore

--- kalev_bot.py
+++ kalev_bot.py
@@ -9,8 +9,6 @@ import discord  # Discord API
 from discord.ext import commands
 from lib import obot
 
-import maincore as dc
-
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(obot.bot_prefix),
                    owner_id=obot.owner_id)
 
@@ -34,7 +32,6 @@ for i in dir_make:
 async def on_ready():
     timer_end = time.time()
     print("Launching of bot took {} seconds".format(timer_start - timer_end))
-    dc.ready(bot)
     await bot.change_presence(activity=discord.Game(type=obot.gametype, name=obot.game),
                               status=discord.Status.online)
     servers = len(bot.guilds)
