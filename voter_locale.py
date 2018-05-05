5cfe2e6394bad21d23da3a44582eecb87d680c34
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-05-05 18:41:46 +0300

    fixed voter locale

--- voter_locale.py
+++ voter_locale.py
@@ -5,7 +5,7 @@ LOCALES = [
         "TIME_HOUR_SG":    "{0} hour",
         "TIME_HOUR_PL":    "{0} hours",
         "TIME_NOW":        "right now",
-        "TIME_AND":        "{0} and {1}",
+        "TIME_AND":        "and",
         "REMINDER_NOW":    "Call starts right now!",
         "REMINDER_REMIND": "Call starts in {0}.",
         "REMINDER_PING":   "Boop! {0} ^",
@@ -18,7 +18,7 @@ LOCALES = [
         "TIME_HOUR_SG":    "ønt {0}",
         "TIME_HOUR_PL":    "ønt {0}",
         "TIME_NOW":        "ønpaj",
-        "TIME_AND":        "{0} da {1}",
+        "TIME_AND":        "da",
         "REMINDER_NOW":    "Doisiu melulumon ønpaj!",
         "REMINDER_REMIND": "Doisiu melulumon øn {0}.",
         "REMINDER_PING":   "Boop! {0} ^",
@@ -26,5 +26,3 @@ LOCALES = [
         "CHECK_PASSED":    "Dandoisiu melulumon {0}.",
     },
 ]
-
-
