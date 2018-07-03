41773f05c6cd7c2fb320e91e13073668d3411d37
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-07-03 20:05:40 +0300

    i didn't realize all the errors in the room lib

--- lib/room.py
+++ lib/room.py
@@ -1,6 +1,8 @@
+import random
+
 class Room:
     def __init__(self, preset):
-        self.desc = preset.get("desc", make_desc())
+        self.desc = preset.get("desc", self.make_desc())
 
     def make_desc(self):
         material = ["brick", "clean brick", "some sort of shiny brick",
@@ -9,5 +11,6 @@ class Room:
                     "stone"]
 
         atmos = ["an eerie", "a wet", "a dry", "a quiet", "a nice"]
-        ident = "The room is made out of " + random.choice(material) + ". It has " + random.choice(atmos) + " atmosphere to it"
+        ident = (f"The room is made out of {random.choice(material)}. "
+                 f"It has {random.choice(atmos)} atmosphere to it")
         self.desc = ident
