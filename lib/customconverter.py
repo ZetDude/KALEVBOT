964a38dcda658e8b413f62a2000059ecac71d086
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-07-29 01:48:55 +0300

    what have i created

--- lib/customconverter.py
+++ lib/customconverter.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 class HybridConverter(commands.Converter):
     async def convert(self, ctx, argument):
@@ -8,76 +9,25 @@ class HybridConverter(commands.Converter):
 
         ### CONVERT TO MEMBER FIRST
         # Lookup by ID
-        got_target = [x for x in all_members if str(x.id) == str(argument)]
+        got_target = ([x for x in all_members if str(x.id) == str(argument)] or
+                      [x for x in all_members if x.mention == str(argument)] or
+                      [x for x in all_members if str(x) == str(argument)] or
+                      [x for x in all_members if x.name == str(argument)] or
+                      [x for x in all_members if x.nick == str(argument)] or
+                      [x for x in all_users if str(x.id) == str(argument)] or
+                      [x for x in all_users if x.mention == str(argument)] or
+                      [x for x in all_users if str(x) == str(argument)] or
+                      [x for x in all_users if x.name == str(argument)] or
+                      [x for x in all_members if str(x).lower() == str(argument).lower()] or
+                      [x for x in all_members if x.name.lower() == str(argument).lower()] or
+                      [x for x in all_members if x.nick.lower() == str(argument).lower()] or
+                      [x for x in all_users if str(x).lower() == str(argument).lower()] or
+                      [x for x in all_users if x.name.lower() == str(argument).lower()] or
+                      None)
         if got_target:
             return got_target[0]
-
-        # Lookup by mention
-        got_target = [x for x in all_members if x.mention == str(argument)]
-        if got_target:
-            return got_target[0]
-
-        # Lookup by name#discrim
-        got_target = [x for x in all_members if str(x) == str(argument)]
-        if got_target:
-            return got_target[0]
-
-        # Lookup by name
-        got_target = [x for x in all_members if x.name == str(argument)]
-        if got_target:
-            return got_target[0]
-
-        # Lookup by nickname
-        got_target = [x for x in all_members if x.nick == str(argument)]
-        if got_target:
-            return got_target[0]
-
-        ### FALLBACK TO GLOBAL USER SEARCH
-        # Lookup by ID
-        got_target = [x for x in all_users if str(x.id) == str(argument)]
-        if got_target:
-            return got_target[0]
-
-        # Lookup by mention
-        got_target = [x for x in all_users if x.mention == str(argument)]
-        if got_target:
-            return got_target[0]
-
-        # Lookup by name#discrim
-        got_target = [x for x in all_users if str(x) == str(argument)]
-        if got_target:
-            return got_target[0]
-
-        # Lookup by name
-        got_target = [x for x in all_users if x.name == str(argument)]
-        if got_target:
-            return got_target[0]
-
-        ### FALLBACK TO CASE-INSENSITIVE MEMBER SEARCH
-        # Lookup by name#discrim
-        got_target = [x for x in all_members if str(x).lower() == str(argument).lower()]
-        if got_target:
-            return got_target[0]
-
-        # Lookup by name
-        got_target = [x for x in all_members if x.name.lower() == str(argument).lower()]
-        if got_target:
-            return got_target[0]
-
-        # Lookup by nickname
-        got_target = [x for x in all_members if x.nick.lower() == str(argument).lower()]
-        if got_target:
-            return got_target[0]
-
-        ### FALLBACK TO CASE-INSENSITIVE GLOBAL USER SEARCH
-        # Lookup by name#discrim
-        got_target = [x for x in all_users if str(x).lower() == str(argument).lower()]
-        if got_target:
-            return got_target[0]
-
-        # Lookup by name
-        got_target = [x for x in all_users if x.name.lower() == str(argument).lower()]
-        if got_target:
-            return got_target[0]
-
-        raise commands.BadArgument(f"Member \"{argument}\" not found")
+        else:
+            try:
+                return await ctx.bot.get_user_info(int(argument))
+            except (ValueError, discord.NotFound):
+                raise commands.BadArgument(f"Member \"{argument}\" not found")
