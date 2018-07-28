319c0115d7bb0d32c9993d50ae16788d0b9cbbf6
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-07-29 02:59:50 +0300

    fix typo

--- cogs/utility.py
+++ cogs/utility.py
@@ -315,7 +315,7 @@ class UtilityCog():
         if not known_as:
             known_as = "I don't know any other nicknames"
         else:
-            known_as = "Also known as" + ", ".join([f'"{x}"' for x in known_as])
+            known_as = "Also known as " + ", ".join([f'"{x}"' for x in known_as])
         if not member:
             if shared:
                 target_user = [x.get_member(target_user.id) for x in ctx.bot.guilds if
