02f806162fa2d5f44f16452d665b11ca5a544975
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-07-29 21:55:35 +0300

    temporary try except

--- lib/customconverter.py
+++ lib/customconverter.py
@@ -6,27 +6,29 @@ class HybridConverter(commands.Converter):
     async def convert(self, ctx, argument, **kwargs):
         all_users = ctx.bot.users
         all_members = ctx.guild.members
-
-        got_target = ([x for x in all_members if str(x.id) == str(argument)] or
-                      [x for x in all_members if x.mention == str(argument)] or
-                      [x for x in all_members if str(x) == str(argument)] or
-                      [x for x in all_members if x.name == str(argument)] or
-                      [x for x in all_members if x.nick == str(argument)] or
-                      [x for x in all_users if str(x.id) == str(argument)] or
-                      [x for x in all_users if x.mention == str(argument)] or
-                      [x for x in all_users if str(x) == str(argument)] or
-                      [x for x in all_users if x.name == str(argument)] or
-                      [x for x in all_members if str(x).lower() == str(argument).lower()] or
-                      [x for x in all_members if x.name.lower() == str(argument).lower()] or
-                      [x for x in all_members if str(x.nick).lower() == str(argument).lower()
-                       and x.nick is not None] or
-                      [x for x in all_users if str(x).lower() == str(argument).lower()] or
-                      [x for x in all_users if x.name.lower() == str(argument).lower()] or
-                      [x for x in [y.members for y in ctx.bot.guilds] if x.nick == str(argument)] or
-                      [x for x in [y.members for y in ctx.bot.guilds] if
-                       str(x.nick).lower() == str(argument).lower() and x.nick is not None] or
-                      difflib.get_close_matches(str(argument), [x.name for x in all_users]) or
-                      None)
+        try:
+            got_target = ([x for x in all_members if str(x.id) == str(argument)] or
+                          [x for x in all_members if x.mention == str(argument)] or
+                          [x for x in all_members if str(x) == str(argument)] or
+                          [x for x in all_members if x.name == str(argument)] or
+                          [x for x in all_members if x.nick == str(argument)] or
+                          [x for x in all_users if str(x.id) == str(argument)] or
+                          [x for x in all_users if x.mention == str(argument)] or
+                          [x for x in all_users if str(x) == str(argument)] or
+                          [x for x in all_users if x.name == str(argument)] or
+                          [x for x in all_members if str(x).lower() == str(argument).lower()] or
+                          [x for x in all_members if x.name.lower() == str(argument).lower()] or
+                          [x for x in all_members if str(x.nick).lower() == str(argument).lower()
+                           and x.nick is not None] or
+                          [x for x in all_users if str(x).lower() == str(argument).lower()] or
+                          [x for x in all_users if x.name.lower() == str(argument).lower()] or
+                          [x for x in [y.members for y in ctx.bot.guilds] if x.nick == str(argument)] or
+                          [x for x in [y.members for y in ctx.bot.guilds] if
+                           str(x.nick).lower() == str(argument).lower() and x.nick is not None] or
+                          difflib.get_close_matches(str(argument), [x.name for x in all_users]) or
+                          None)
+        except Exception as e:
+            await ctx.send(e)
         if got_target:
             return got_target[0]
         else:
