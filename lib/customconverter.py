1cd8d2f9a7ef45513d735f278baae42533cc35ef
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-07-29 21:46:54 +0300

    fuzzier matching in HybridConverter

--- lib/customconverter.py
+++ lib/customconverter.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
 import discord
+import difflib
 
 class HybridConverter(commands.Converter):
-    async def convert(self, ctx, argument):
+    async def convert(self, ctx, argument, **kwargs):
         all_users = ctx.bot.users
         all_members = ctx.guild.members
 
@@ -21,6 +22,10 @@ class HybridConverter(commands.Converter):
                        and x.nick is not None] or
                       [x for x in all_users if str(x).lower() == str(argument).lower()] or
                       [x for x in all_users if x.name.lower() == str(argument).lower()] or
+                      [x for x in [y.members for y in ctx.bot.guilds] if x.nick == str(argument)] or
+                      [x for x in [y.members for y in ctx.bot.guilds] if
+                       str(x.nick).lower() == str(argument).lower() and x.nick is not None] or
+                      difflib.get_close_matches(str(argument), [x.name for x in all_users]) or
                       None)
         if got_target:
             return got_target[0]
