ff7fa17dc0b2690607314b604fbd473d15fcfb70
ZetDude <31309189+ZetDude@users.noreply.github.com>
2018-08-04 23:33:43 +0300

    less print noise

--- relay.py
+++ relay.py
@@ -1,10 +1,11 @@
 import pickle
 import pprint
 import discord
+import asyncio
 from discord.ext import commands
 from lib import obot
-pp = pprint.PrettyPrinter(indent=2)
 
+pp = pprint.PrettyPrinter(indent=2)
 bot = commands.Bot(command_prefix='k!relay ')
 
 RELAYFILE = 'important/relay.pickle'
@@ -17,6 +18,7 @@ CREATE_MESSAGE = ("**Created relay data for the server `{ctx.guild.name}`**\n"
 JOIN_MESSAGE = ("Currently, all your info such as the name of your conlang and "
                 "the order you are in are not set. To configure this, use "
                 "`{ctx.prefix}edit` to begin the configuration wizard.")
+SKIP = "skip"
 
 DEFAULT_SERVER = {"state": 0,
                   "part": {},
@@ -38,6 +40,11 @@ except FileNotFoundError:
 async def write_data(to_write):
     with open(RELAYFILE, 'wb') as f:
         pickle.dump(to_write, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+async def relay_part(part_dict):
+    part_list = [(key, value["lang"], value["order"]) for key, value in part_dict.items()]
+    
+
 
 @bot.event
 async def on_ready():
@@ -85,21 +92,60 @@ async def role(ctx, *, role_name):
         await ctx.send(f"{ctx.author.name}, please give me permission to assign roles before doing that.")
     elif discord.utils.get(ctx.guild.roles, name=role_name) is None:
         await ctx.send(f"{ctx.author.name}, couldn't find a role named that.")
-    elif server_data.get(ctx.guild.id, None) is None:
+    elif server_data.get(ctx.guild.id) is None:
         await ctx.send(f"{ctx.author.name}, no relay made for this server ({ctx.prefix}register).")
-    players = list(server_data[ctx.guild.id]["part"])
-    players = [x for x in ctx.guild.members if x.id in players]
-    server_data[ctx.guild.id]["role"] = role_name
-    add_role = discord.utils.get(ctx.guild.roles, name=role_name)
-    for player in players:
-        await ctx.send(f"going back to add {add_role} to {player}")
-        await player.add_roles(add_role)
-    await write_data(server_data)
+    else:
+        players = list(server_data[ctx.guild.id]["part"])
+        players = [x for x in ctx.guild.members if x.id in players]
+        server_data[ctx.guild.id]["role"] = role_name
+        add_role = discord.utils.get(ctx.guild.roles, name=role_name)
+        for player in players:
+            await player.add_roles(add_role)
+        await write_data(server_data)
+
+@bot.command(name='edit', aliases=['configure'])
+async def edit(ctx):
+    def dm_check(msg):
+        return msg.channel == ctx.author.dm_channel
+
+    if server_data.get(ctx.guild.id) is None:
+        await ctx.send(f"{ctx.author.name}, no relay made for this server ({ctx.prefix}register).")
+    elif server_data[ctx.guild.get]["part"].get(ctx.author.id) is None:
+        await ctx.send(f"{ctx.author.name}, you haven't joined the relay ({ctx.prefix}register).")
+    else:
+        try:
+            if ctx.guild is not None:
+                await ctx.message.add_reaction("\U0001F538")
+            await ctx.author.send((f"Starting relay config wizard.\n"
+                                   f"Reply with the name of your conlang. `{SKIP}` to skip this."))
+            msg = await bot.wait_for('message', check=dm_check, timeout=60.0)
+            if msg.lower() != SKIP:
+                if len(msg.content) > 40:
+                    await ctx.author.send("Language name cannot be longer than 40 characters")
+                else:
+                    server_data[ctx.guild.get]["part"][ctx.author.id]["lang"] = msg.content
+                    await ctx.author.send(f"Successfully set your language to `{msg.content}`")
+
+            await ctx.author.send(f"Reply with the position you want. `{SKIP}` to skip this.")
+            msg = await bot.wait_for('message', check=dm_check, timeout=60.0)
+            if msg.lower() != SKIP:
+                if len(msg.content) > 40:
+                    await ctx.author.send("Language name cannot be longer than 40 characters")
+                else:
+                    server_data[ctx.guild.get]["part"][ctx.author.id]["lang"] = msg.content
+                    await ctx.author.send(f"Successfully set your language to `{msg.content}`")
+
+
+        except asyncio.TimeoutError:
+            await ctx.author.send(f"You took too long to respond. Cancelling")
+            if ctx.guild is not None:
+                await ctx.message.remove_reaction("\U0001F538", ctx.bot.user)
+                await ctx.message.add_reaction("\u274C")
+
 
 @bot.command(name='data', aliases=[])
 async def data(ctx):
     formatted = pp.pformat(server_data)
     await ctx.send(f"```\n{formatted}\n```")
 
-bot.run(obot.token)
-
+bot.run(obot.BOT_TOKEN)
